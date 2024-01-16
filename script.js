cd21c935c1fb5e94a5e13ac5627ffe763e6ad18c
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2024-01-16 11:54:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.querySelectorAll('.thumbnail').forEach(item => {
+    item.addEventListener('click', event => {
+        let fullSizeContainer = document.getElementById('fullsizeContainer');
+        let fullSizeImage = document.getElementById('fullsize');
+        fullSizeImage.src = item.src;
+        fullSizeContainer.style.display = 'flex';
+    });
+});
+
+document.getElementById('fullsizeContainer').addEventListener('click', () => {
+    document.getElementById('fullsizeContainer').style.display = 'none';
+});
